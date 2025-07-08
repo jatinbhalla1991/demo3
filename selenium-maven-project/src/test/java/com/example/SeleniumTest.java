3fb83dfc5861ce236d68111a3c1e74556a9a4ba0
jatinbhalla <jatinbhalla18.jb@gmail.com>
2025-07-08 11:01:12 +0530

    selinuim test

--- selenium-maven-project/src/test/java/com/example/SeleniumTest.java
+++ selenium-maven-project/src/test/java/com/example/SeleniumTest.java
@@ -1,22 +1,36 @@
 package com.example;
 
-import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import io.github.bonigarcia.wdm.WebDriverManager; // <-- Add this import
 
 public class SeleniumTest {
+    private WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        WebDriverManager.chromedriver().setup(); // <-- Use WebDriverManager
+        driver = new ChromeDriver();
+    }
 
     @Test
-    public void openGoogle() {
-        // Path to chromedriver (skip this if chromedriver is in your PATH)
-        System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
-
-        WebDriver driver = new ChromeDriver();
-
+    public void testGoogleSearch() {
         driver.get("https://www.google.com");
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys("Selenium");
+        searchBox.submit();
+        System.out.println("Page title is: " + driver.getTitle());
+    }
 
-        System.out.println("Title: " + driver.getTitle());
-
-        driver.quit();
+    @AfterEach
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
